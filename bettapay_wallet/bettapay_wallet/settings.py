5ea009cec118046e961c23ac5d1482880d44315c
agarjoshua <joshuaagarjj@gmail.com>
2021-03-22 10:21:10 +0300

    Moved the database from postgre to mysqlite

--- bettapay_wallet/bettapay_wallet/settings.py
+++ bettapay_wallet/bettapay_wallet/settings.py
@@ -76,22 +76,22 @@ WSGI_APPLICATION = 'bettapay_wallet.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bitcoinwallet',
-        'USER': 'postgres',
-        'PASSWORD': 'bhurjkhalifa',
-        'HOST': 'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'bitcoinwallet',
+#         'USER': 'postgres',
+#         'PASSWORD': 'bhurjkhalifa',
+#         'HOST': 'localhost'
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
